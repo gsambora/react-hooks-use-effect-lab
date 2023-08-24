1b00545f7e16536598decc9a23f5f440db4f8b9f
Hana Sambora <grace.sambora@gmail.com>
2023-08-24 01:45:35 -0500

    Complete lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(()=>{
+    const questionTimer = setTimeout(()=>{
+      //console.log("running "+timeRemaining);
+      if(timeRemaining>1){ 
+        setTimeRemaining(timeRemaining-1);
+      } else {
+        //console.log(timeRemaining)
+        setTimeRemaining(10);
+        onAnswered(false);
+      }
+    }, 1000);
+
+    return function cleanup() {
+      clearTimeout(questionTimer);
+    }
+  })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
